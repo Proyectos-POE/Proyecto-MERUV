d46c74ea51a062d5d1ee066c0ce8ede3d4097ed6
SakyJoestar <itzsaky@outlook.com>
2023-01-04 18:07:26 -0500

    Se creó la clase ProveedorDao con sus respectivos atributos y metodos, ademas añadi los autores al codigo.

--- src/main/java/modelo/Proveedor.java
+++ src/main/java/modelo/Proveedor.java
@@ -1,6 +1,11 @@
 package modelo;
 import java.io.Serializable;
 
+/*
+ * @author Nicolas Herrera <herrera.nicolas@correounivalle.edu.co>
+ * @author Samuel Galindo Cuevas <samuel.galindo@correounivalle.edu.co>
+ * @author Julian Rendon <julian.david.rendon@correounivalle.edu.co>
+ */
 
 public class Proveedor implements Serializable
 {
