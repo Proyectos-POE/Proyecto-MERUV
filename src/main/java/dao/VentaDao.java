8a194a277dd5030269352569f9634d7267b8dea9
Julian <juldarecar2003@gmail.com>
2023-01-05 16:22:21 -0500

    MERUV-10-crear-clase-venta-dao Cree la clase VentaDao

---
+++ src/main/java/dao/VentaDao.java
@@ -0,0 +1,63 @@
+package dao;
+
+import modelo.Venta;
+
+import java.util.ArrayList;
+
+/*
+ * @author Nicolas Herrera <herrera.nicolas@correounivalle.edu.co>
+ * @author Samuel Galindo Cuevas <samuel.galindo@correounivalle.edu.co>
+ * @author Julian Rendon <julian.david.rendon@correounivalle.edu.co>
+ */
+
+public class VentaDao 
+{
+    private final ArrayList<Venta> ventas;
+    
+    public VentaDao()
+    {
+        ventas = new ArrayList <Venta>();
+    }
+    
+    public Venta getVenta(int auxId)
+    {
+        Venta auxVenta = null;
+        for(Venta venta: ventas)
+        {
+            if(venta.getId() == auxId)
+            {
+                auxVenta = venta;
+                break;
+            }
+        }
+        return auxVenta;
+    }
+    
+    public boolean anhadirVenta(Venta auxVenta)
+    {
+        ventas.add(auxVenta);
+        return true;
+    }
+    
+    public boolean eliminarProducto(Venta auxVenta)
+    {
+        ventas.remove(auxVenta);
+        return true;
+    }
+    
+    public boolean actualizarProducto(Venta auxVenta)
+    {
+        if(ventas.contains(auxVenta))
+        {
+            int pos = ventas.indexOf(auxVenta);
+            ventas.set(pos, auxVenta);
+            return true; 
+        }
+        return false;
+    }
+    
+    public ArrayList<Venta> getVentas()
+    {
+        return ventas;
+    }
+}
